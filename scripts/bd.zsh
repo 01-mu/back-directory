d0d9840e5374d4a9a5a851a71d804c6639af5779
mu <masato.umeno.818@gmail.com>
2026-01-01 11:53:33 +0900

    bd: add h as primary help alias

--- scripts/bd.zsh
+++ scripts/bd.zsh
@@ -88,15 +88,16 @@ bd() {
     arg=1
   fi
 
-  if [[ $arg == "help" || $arg == "-h" || $arg == "--help" ]]; then
+  if [[ $arg == "h" || $arg == "help" || $arg == "-h" || $arg == "--help" ]]; then
     cat <<'EOF'
-usage: bd [N|c|ls|help]
+usage: bd [N|c|ls|h]
 
   bd           go back 1 directory
   bd N         go back N directories (1 <= N <= 999)
   bd c         cancel the last bd command
   bd ls [N]    list recent targets with their N values (default 10)
-  bd help      show this help
+  bd h         show this help
+  bd help      alias for bd h
 EOF
     return 0
   fi
@@ -138,7 +139,7 @@ EOF
   fi
 
   if [[ $arg != <-> || $arg -le 0 ]]; then
-    print -r -- "bd: usage: bd [N|c|ls]"
+    print -r -- "bd: usage: bd [N|c|ls|h]"
     return 1
   fi
 
